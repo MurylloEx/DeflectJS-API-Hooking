5c57388752d7be1476dc30a5de9047cd0850e717
Muryllo Pimenta <32225687+MurylloEx@users.noreply.github.com>
2020-01-04 18:02:27 -0300

    Added new feature: native hooks!
    Native hooks added for support to native functions like:
    
    window.alert;
    document.write;
    document.createElement;
    console.log.
    ...

--- deflectorjs/deflector.js
+++ deflectorjs/deflector.js
@@ -1,7 +1,10 @@
 /** Hook types */
-const DEFLECT_PRE_OPERATION_HOOK    = 0x01;
-const DEFLECT_OVERWRITTEN_HOOK      = 0x02;
-const DEFLECT_POST_OPERATION_HOOK   = 0x04;
+const DEFLECT_PRE_OPERATION_HOOK            = 0x01;
+const DEFLECT_OVERWRITTEN_HOOK              = 0x02;
+const DEFLECT_POST_OPERATION_HOOK           = 0x04;
+const DEFLECT_NATIVE_PRE_OPERATION_HOOK     = 0x08;
+const DEFLECT_NATIVE_OVERWRITTEN_HOOK       = 0x10;
+const DEFLECT_NATIVE_POST_OPERATION_HOOK    = 0x20;
 
 /** Return code of deflect api calls */
 const DEFLECT_STATE_WAITING         = 0x01;
@@ -24,20 +27,24 @@ function deflect_create_hook(targFunc, newFunc, hookType, parent){
     if (typeof parent == "undefined" || parent == null){
         parent = window;
     }
+    if (typeof hookType == "undefined"){
+        hookType = DEFLECT_OVERWRITTEN_HOOK;
+    }
     switch (hookType) {
         case DEFLECT_PRE_OPERATION_HOOK:
-            stub = function() { newFunc.apply(this, arguments); return targFunc.apply(this, arguments); }
+            stub = function defprehookstub() { newFunc.apply(this, arguments); return targFunc.apply(this, arguments); }
             break;
         case DEFLECT_OVERWRITTEN_HOOK:
-            stub = function() { return newFunc.apply(this, arguments); }
+            stub = function defoverhookstub() { return newFunc.apply(this, arguments); }
             break;
         case DEFLECT_POST_OPERATION_HOOK:
-            stub = function() { let result = targFunc.apply(this, arguments); newFunc.apply(this, arguments); return result; }
+            stub = function defposthookstub() { let result = targFunc.apply(this, arguments); newFunc.apply(this, arguments); return result; }
             break;
         default:
-            stub = function() { return newFunc.apply(this, arguments); }
+            stub = function defoverhookstub() { return newFunc.apply(this, arguments); }
             break;
     }
+    Object.defineProperty(stub, "name", {value: oldFunc.name});
     return {
         OriginalFunction: oldFunc,
         OriginalFunctionName: oldFunc.name,
@@ -49,6 +56,66 @@ function deflect_create_hook(targFunc, newFunc, hookType, parent){
         PatchAddr: targFunc,
         Guid: null
     };
+}
+
+/**Create a hook structure to be queued and after attached.
+ * 
+ * @param {*} targFunc Target function to be hooked.
+ * @param {*} newFunc New function that will replace the target function.
+ * @param {*} hookType The hook type (DEFLECT_NATIVE_PRE_OPERATION_HOOK, DEFLECT_NATIVE_OVERWRITTEN_HOOK or DEFLECT_NATIVE_POST_OPERATION_HOOK).
+ * @param {*} parent The parent object of target function (default: window).
+ */
+function deflect_create_native_hook(targFunc, newFunc, hookType, parent){
+    let oldFunc = targFunc;
+    let struct = {
+        OriginalFunction: oldFunc,
+        OriginalFunctionName: oldFunc.name,
+        NewFunction: newFunc,
+        Parent: parent,
+        HookType: hookType,
+        HookState: DEFLECT_STATE_WAITING,
+        HookStub: function(){},
+        PatchAddr: targFunc,
+        Guid: null
+    }
+    if (typeof parent == "undefined" || parent == null){
+        parent = window;
+    }
+    if (typeof hookType == "undefined"){
+        hookType = DEFLECT_NATIVE_OVERWRITTEN_HOOK;
+    }
+    switch (struct.HookType) {
+        case DEFLECT_NATIVE_PRE_OPERATION_HOOK:
+            struct.HookStub = function defntprehookstub() {
+                newFunc.apply(this, arguments);
+                deflect_unpatch_original(struct);
+                let result = struct.Parent[struct.OriginalFunctionName].apply(this, arguments);
+                deflect_patch_original(struct);
+                return result;
+            }
+            break;
+        case DEFLECT_NATIVE_OVERWRITTEN_HOOK:
+            struct.HookStub = function defntoverhookstub() {
+                return newFunc.apply(this, arguments);
+            }
+            break;
+        case DEFLECT_NATIVE_POST_OPERATION_HOOK:
+            struct.HookStub = function defntposthookstub() {
+                deflect_unpatch_original(struct);
+                let result = struct.Parent[struct.OriginalFunctionName].apply(this, arguments);
+                deflect_patch_original(struct);
+                newFunc.apply(this, arguments);
+                return result;
+            }
+            break;
+        default:
+            struct.HookStub = function defntoverhookstub() {
+                return newFunc.apply(this, arguments);
+            }
+            break;
+    }
+    Object.defineProperty(struct.HookStub, "name", {value: oldFunc.name});
+    return struct;
 }
 
 /**Generate a GUID (Global Unique Identifier) for hooks.
@@ -147,5 +214,39 @@ function deflect_detach_hook(queueHook){
         queueHook[k].Parent[queueHook[k].OriginalFunctionName] = queueHook[k].OriginalFunction;
         queueHook[k].HookState = DEFLECT_STATE_UNHOOKED;
     }
+    return DEFLECT_STATE_UNHOOKED;
+}
+
+/**Patch the original function.
+ * 
+ * @param {*} hookStruct Hook structure returned in deflect_create_hook(...) function.
+ */
+function deflect_patch_original(hookStruct){
+    if (hookStruct.HookType != DEFLECT_NATIVE_PRE_OPERATION_HOOK &&
+        hookStruct.HookType != DEFLECT_NATIVE_OVERWRITTEN_HOOK &&
+        hookStruct.HookType != DEFLECT_NATIVE_POST_OPERATION_HOOK) {
+        hookStruct.HookState = DEFLECT_STATE_FAILED;
+        return DEFLECT_STATE_FAILED;
+    }
+    hookStruct.OriginalFunction = hookStruct.Parent[hookStruct.OriginalFunctionName];
+    hookStruct.Parent[hookStruct.OriginalFunctionName] = hookStruct.HookStub;
+    hookStruct.HookState = DEFLECT_STATE_HOOKED;
+    return DEFLECT_STATE_HOOKED;
+}
+
+/**Unpatch the original function.
+ * 
+ * @param {*} hookStruct Hook structure returned in deflect_create_hook(...) function.
+ */
+function deflect_unpatch_original(hookStruct){
+    if (hookStruct.HookType != DEFLECT_NATIVE_PRE_OPERATION_HOOK &&
+        hookStruct.HookType != DEFLECT_NATIVE_OVERWRITTEN_HOOK &&
+        hookStruct.HookType != DEFLECT_NATIVE_POST_OPERATION_HOOK) {
+        hookStruct.HookState = DEFLECT_STATE_FAILED;
+        return DEFLECT_STATE_FAILED;
+    }
+    hookStruct.HookStub = hookStruct.Parent[hookStruct.OriginalFunctionName];
+    hookStruct.Parent[hookStruct.OriginalFunctionName] = hookStruct.OriginalFunction;
+    hookStruct.HookState = DEFLECT_STATE_UNHOOKED;
     return DEFLECT_STATE_UNHOOKED;
 }
